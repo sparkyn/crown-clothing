7ebb062b1636e95ed0a95de15aee53b996e9800b
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2019-12-13 13:49:57 +0000

    Added React Router DOM to project, wrapped App in BrowserRouter, added Switch and Routes to components, made MenuItem a Higher Order Component using withRouter()

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div className={`${size} menu-item`}>
-        <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
+        <div 
+            className="background-image" 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
         />
-        <div className="content">
+        <div 
+            className="content"
+            style={{
+                pointerEvents: 'none'
+            }}
+        >
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
