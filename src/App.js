249d56d94a6011b4c302641701ebfffdf51b3200
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2020-02-12 15:28:15 +0000

    Implement Firebase utils including ability to store authenticated users. NOT convinced this is working properly in Firebase.

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ class App extends React.Component {
                id: snapShot.id,
                ...snapShot.data()
              }
-           }, () => {
-            console.log(this.state);
            });
+
+           console.log(this.state);
         });
         
       }
